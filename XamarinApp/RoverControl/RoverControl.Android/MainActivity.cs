5bc3f8c36300816fcdb1486f749125e7836b33f3
paulthedev <paulthedev@hotmail.com>
2019-06-09 22:09:57 +0530

    Initial Commit for Xamarin App

---
+++ XamarinApp/RoverControl/RoverControl.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using nexus.protocols.ble;
+
+namespace RoverControl.Droid
+{
+    [Activity(Label = "RoverControl", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            // If you want to enable/disable the Bluetooth adapter from code, you must call this.
+            BluetoothLowEnergyAdapter.Init(this);
+            // Obtain the bluetooth adapter so we can pass it into our (shared-code) Xamarin Forms app. There are
+            // additional Obtain() methods on BluetoothLowEnergyAdapter if you have more specific needs (e.g. if you
+            // need to support devices with multiple Bluetooth adapters)
+            var bluetooth = BluetoothLowEnergyAdapter.ObtainDefaultAdapter(ApplicationContext);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App(bluetooth));
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ XamarinApp/RoverControl/RoverControl/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RoverControl.Services;
+using RoverControl.Views;
+using nexus.protocols.ble;
+
+namespace RoverControl
+{
+    public partial class App : Application
+    {
+
+        public App(IBluetoothLowEnergyAdapter ble)
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
