3d08b950a60c2fdc1611f878477106628950fe5b
paulthedev <paulthedev@hotmail.com>
2019-06-12 19:53:01 +0530

    Add request for runtime permissions in Android

--- XamarinApp/RoverControl/RoverControl.Android/MainActivity.cs
+++ XamarinApp/RoverControl/RoverControl.Android/MainActivity.cs
@@ -7,11 +7,13 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using nexus.protocols.ble;
+using Android.Support.V4.App;
+using Android;
 
 namespace RoverControl.Droid
 {
     [Activity(Label = "RoverControl", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -20,8 +22,14 @@ namespace RoverControl.Droid
 
             base.OnCreate(savedInstanceState);
 
-            // If you want to enable/disable the Bluetooth adapter from code, you must call this.
-            BluetoothLowEnergyAdapter.Init(this);
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.AccessCoarseLocation))
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.AccessCoarseLocation }, 0);
+            }
+            else if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.AccessFineLocation))
+            { 
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.AccessFineLocation }, 1);
+            }
             // Obtain the bluetooth adapter so we can pass it into our (shared-code) Xamarin Forms app. There are
             // additional Obtain() methods on BluetoothLowEnergyAdapter if you have more specific needs (e.g. if you
             // need to support devices with multiple Bluetooth adapters)
--- XamarinApp/RoverControl/RoverControl.Android/Properties/AssemblyInfo.cs
+++ XamarinApp/RoverControl/RoverControl.Android/Properties/AssemblyInfo.cs
@@ -35,4 +35,5 @@ using Android.App;
 [assembly: UsesPermission(Android.Manifest.Permission.Bluetooth)]
 [assembly: UsesPermission(Android.Manifest.Permission.BluetoothAdmin)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
 [assembly: UsesPermission(Android.Manifest.Permission.Vibrate)]
